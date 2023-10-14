49ef420b4f3dacdebf78b262eb7dfa3086c3155f
h585911 <585911@stud.hvl.no>
2023-10-14 16:45:42 +0200

    Ferdig med KortUtils.java

--- src/no/hvl/dat100/prosjekt/modell/KortUtils.java
+++ src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -1,5 +1,6 @@
 package no.hvl.dat100.prosjekt.modell;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import no.hvl.dat100.prosjekt.TODO;
@@ -19,8 +20,34 @@ public class KortUtils {
 		
 		// TODO - START
 		
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 		// TODO - END
+		
+		/*Kort[] kort = samling.getAllekort();
+		int n = kort.length;
+		boolean sortert = false;
+		
+		while (!sortert) {
+			sortert = true;
+			for (int i = 0; i < n - 1; i++) {
+				if (kort[i].compareTo(kort[i + 1]) > 0) {
+					Kort temp = kort[i];
+					kort[i] = kort[i + 1];
+					kort[i + 1] = temp;
+					sortert = false;
+				}
+			}
+			
+			n--;
+		}*/
+		Kort[] kort = samling.getAllekort();
+		Arrays.sort(kort);
+		for (int i = 0; i < kort.length; i++) {
+			samling.fjernAlle();
+			for (Kort k : kort) {
+				samling.leggTil(k);
+			}
+		}
 	}
 	
 	/**
@@ -33,8 +60,17 @@ public class KortUtils {
 		
 		// TODO - START
 		
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
 		// TODO - END
+		Kort[] kort = samling.getAllekort();
+		Random rand = new Random();
+		
+		for (int i = kort.length - 1; i > 0; i--) {
+			int j = rand.nextInt(i + 1);
+			Kort temp = kort[i];
+			kort[i] = kort[j];
+			kort[j] = temp;
+		}
 	}
 	
 }
